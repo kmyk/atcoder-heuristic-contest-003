c8d88662cf3a7b59a5085163faf1b5f8f56bd6f1
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-05-24 05:32:59 +0900

    scripts/dashboard.sh を更新する

--- scripts/dashboard.sh
+++ scripts/dashboard.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 k=${1:-50}
-mkdir log
-for M in 1 2 ; do
-    for D in 100 300 600 900 1200 1500 1800 2000 ; do
+mkdir -p log
+for D in 100 300 600 900 1200 1500 1800 2000 ; do
+    for M in 1 2 ; do
         average=$(python3 scripts/measure.py -j6 --count=$k -M $M -D $D 2>&1 | tee log/M${M}D${D}.txt | tail -n 1 | cut -d = -f 2)
         echo M = $M, D = $D: $average
     done
