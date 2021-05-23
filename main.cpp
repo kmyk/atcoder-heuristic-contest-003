60107e101e1ffae9671bc1db2c32375f5c9bd473
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-05-23 19:21:38 +0900

    使ってないコードをまとめて削除する

--- main.cpp
+++ main.cpp
@@ -58,96 +58,6 @@ string get_command_from_path(const vector<pair<int, int>>& path) {
 const int DY[4] = {-1, 1, 0, 0};
 const int DX[4] = {0, 0, 1, -1};
 
-template <class RandomEngine>
-vector<pair<int, int>> solve_with_random_snake(int sy, int sx, int ty, int tx, RandomEngine& gen) {
-    assert (sy <= ty);
-    assert (sx <= tx);
-
-    array<array<bool, W - 1>, H> hr = {};
-    array<array<bool, H - 1>, W> vr = {};
-    int count = 0;
-    REP3 (x, sx, tx) {
-        hr[sy][x] = true;
-        count += 1;
-    }
-    REP3 (y, sy, ty) {
-        vr[tx][y] = true;
-        count += 1;
-    }
-    auto is_visited = [&](int y, int x) -> bool {
-        return (x < W - 1 and hr[y][x]) or (x - 1 >= 0 and hr[y][x - 1]) or (y < H - 1 and vr[x][y]) or (y - 1 >= 0 and vr[x][y - 1]);
-    };
-    REP (iteration, 100000) {
-        int y = uniform_int_distribution<int>(0, H - 2)(gen);
-        int x = uniform_int_distribution<int>(0, W - 2)(gen);
-        bool& hr1 = hr[y][x];
-        bool& hr2 = hr[y + 1][x];
-        bool& vr1 = vr[x][y];
-        bool& vr2 = vr[x + 1][y];
-
-        int ke = hr1 + hr2 + vr1 + vr2;
-        int kv = is_visited(y, x) + is_visited(y, x + 1) + is_visited(y + 1, x) + is_visited(y + 1, x + 1);
-        bool ok = false;
-        if (ke == 0) {
-            // nop
-        } else if (ke == 1) {
-            if (kv == 2) {
-                ok = true;
-            }
-        } else if (ke == 2) {
-            if (hr1 != hr2 and vr1 != vr2) {
-                if (kv == 3) {
-                    ok = true;
-                }
-            }
-        } else if (ke == 3) {
-            ok = true;
-        } else if (ke == 4) {
-            // nop
-        } else {
-            assert (false);
-        }
-
-        if (ok) {
-            hr1 = not hr1;
-            hr2 = not hr2;
-            vr1 = not vr1;
-            vr2 = not vr2;
-
-            count -= ke;
-            count += 4 - ke;
-        }
-        if (count >= 4 * H) {
-            break;
-        }
-    }
-
-    vector<pair<int, int>> path;
-    int y = sy;
-    int x = sx;
-    path.emplace_back(y, x);
-    while (not (y == ty and x == tx)) {
-        if (x < W - 1 and hr[y][x]) {
-            hr[y][x] = false;
-            x += 1;
-        } else if (x - 1 >= 0 and hr[y][x - 1]) {
-            hr[y][x - 1] = false;
-            x -= 1;
-        } else if (y < H - 1 and vr[x][y]) {
-            vr[x][y] = false;
-            y += 1;
-        } else if (y - 1 >= 0 and vr[x][y - 1]) {
-            vr[x][y - 1] = false;
-            y -= 1;
-        } else {
-            assert (false);
-        }
-        path.emplace_back(y, x);
-    }
-    assert (path.size() == count + 1);
-    return path;
-}
-
 vector<pair<int, int>> solve_with_dijkstra(int sy, int sx, int ty, int tx, const array<array<int64_t, W - 1>, H>& hr, const array<array<int64_t, H - 1>, W>& vr) {
     reversed_priority_queue<tuple<int64_t, int, int>> que;
     array<array<double, W>, H> dist;
@@ -194,129 +104,6 @@ vector<pair<int, int>> solve_with_dijkstra(int sy, int sx, int ty, int tx, const
     return path;
 }
 
-array<array<int64_t, W - 1>, H> make_costs_from_base(const array<int64_t, H>& row) {
-    array<array<int64_t, W - 1>, H> hr;
-    REP (y, H) {
-        fill(ALL(hr[y]), row[y]);
-    }
-    return hr;
-}
-
-template <class RandomEngine>
-array<array<int64_t, W - 1>, H> make_costs_from_base_with_random(const array<int64_t, H>& row, RandomEngine& gen) {
-    array<array<int64_t, W - 1>, H> hr;
-    REP (y, H) {
-        REP (x, W - 1) {
-            hr[y][x] = row[y] + uniform_int_distribution<int>(0, 300)(gen);
-        }
-    }
-    return hr;
-}
-
-class path_composer {
-    vector<pair<int, int>> moves;
-
-public:
-    path_composer(int sy, int sx) {
-        assert (0 <= sy and sy < H);
-        assert (0 <= sx and sx < W);
-        moves.emplace_back(sy, sx);
-    }
-
-    vector<pair<int, int>> operator *() const {
-        vector<pair<int, int>> path;
-        for (auto [ty, tx] : moves) {
-            if (path.empty()) {
-                path.emplace_back(ty, tx);
-            }
-            auto [y, x] = path.back();
-            if (y == ty) {
-                while (x != tx)  {
-                    x += (x < tx ? 1 : -1);
-                    path.emplace_back(y, x);
-                }
-            } else if (x == tx) {
-                while (y != ty)  {
-                    y += (y < ty ? 1 : -1);
-                    path.emplace_back(y, x);
-                }
-            } else {
-                assert (false);
-            }
-        }
-        return path;
-    }
-
-    // A `moves` is the sequence of the corners of a `path`.
-    vector<pair<int, int>> get_moves() const {
-        return moves;
-    }
-
-    operator bool() const {
-        array<array<bool, W>, H> used = {};
-        for (auto [y, x] : **this) {
-            if (used[y][x]) {
-                return false;
-            }
-            used[y][x] = true;
-        }
-        return true;
-    }
-
-    void go_up() {
-        auto [y, x] = moves.back();
-        assert (y - 1 >= 0);
-        moves.emplace_back(y - 1, x);
-    }
-
-    void go_down() {
-        auto [y, x] = moves.back();
-        assert (y + 1 < H);
-        moves.emplace_back(y + 1, x);
-    }
-
-    void go_right() {
-        auto [y, x] = moves.back();
-        assert (x + 1 < W);
-        moves.emplace_back(y, x + 1);
-    }
-
-    void go_left() {
-        auto [y, x] = moves.back();
-        assert (x - 1 >= 0);
-        moves.emplace_back(y, x - 1);
-    }
-
-    void go_row(int ty) {
-        assert (0 <= ty and ty < H);
-        auto [y, x] = moves.back();
-        moves.emplace_back(ty, x);
-    }
-
-    void go_col(int tx) {
-        assert (0 <= tx and tx < W);
-        auto [y, x] = moves.back();
-        moves.emplace_back(y, tx);
-    }
-};
-
-int64_t calculate_score(const vector<pair<int, int>>& moves, const array<int64_t, H>& row, const array<int64_t, W>& col) {
-    assert (not moves.empty());
-    int64_t score = 0;
-    REP (i, moves.size() - 1) {
-        auto [ay, ax] = moves[i];
-        auto [by, bx] = moves[i + 1];
-        if (ay == by) {
-            score += row[ay] * abs(bx - ax);
-        } else if (ax == bx) {
-            score += col[ax] * abs(by - ay);
-        } else {
-            assert (false);
-        }
-    }
-    return score;
-}
-
 int64_t calculate_score(const vector<pair<int, int>>& path, const array<array<int64_t, W - 1>, H>& hr, const array<array<int64_t, H - 1>, W>& vr) {
     assert (not path.empty());
     int64_t score = 0;
@@ -334,107 +121,6 @@ int64_t calculate_score(const vector<pair<int, int>>& path, const array<array<in
     return score;
 }
 
-vector<pair<int, int>> solve_with_simple(int sy, int sx, int ty, int tx) {
-    path_composer path(sy, sx);
-    path.go_row(ty);
-    path.go_col(tx);
-    return *path;
-}
-
-template <class RandomEngine>
-vector<pair<int, int>> solve_with_random(int sy, int sx, int ty, int tx, RandomEngine& gen) {
-    assert (sy <= ty);
-    assert (sx <= tx);
-
-    path_composer path(sy, sx);
-    while (true) {
-        auto [y, x] = path.get_moves().back();
-        if (y >= ty or x >= tx) {
-            break;
-        }
-
-        int r = uniform_int_distribution<int>(0, ty - y + tx - x - 1)(gen);
-        if (r < ty - y) {
-            path.go_down();
-        } else {
-            path.go_right();
-        }
-    }
-    path.go_row(ty);
-    path.go_col(tx);
-    assert (path);
-    return *path;
-}
-
-vector<pair<int, int>> solve_with_scan(int sy, int sx, int ty, int tx, int scan_y) {
-    assert (sy <= ty);
-    assert (sx <= tx);
-    assert (scan_y != sy and scan_y != ty);
-
-    path_composer path(sy, sx);
-    path.go_col(0);
-    path.go_row(scan_y);
-    path.go_col(W - 1);
-    path.go_row(ty);
-    path.go_col(tx);
-    assert (path);
-    return *path;
-}
-
-vector<pair<int, int>> solve_with_three(int sy, int sx, int ty, int tx, const array<int64_t, H>& row, const array<int64_t, W>& col) {
-    assert (sy <= ty);
-    assert (sx <= tx);
-
-    vector<path_composer> cands;
-    int64_t highscore = -1;
-    auto use = [&](const path_composer& path) -> void {
-        int64_t score = calculate_score(path.get_moves(), row, col);
-        if (score <= highscore) {
-            highscore = score;
-            cands.emplace_back(path);
-        }
-    };
-
-    REP3 (y1, max(0, sy - 3), min(H, ty + 3 + 1)) {
-        REP3 (x, max(0, sx - 3), min(W, tx + 3 + 1)) {
-            REP3 (y2, y1, min(H, ty + 3 + 1)) {
-                path_composer path(sy, sx);
-                path.go_row(y1);
-                path.go_col(x);
-                path.go_row(y2);
-                path.go_col(tx);
-                path.go_row(ty);
-                use(path);
-            }
-        }
-    }
-
-    REP3 (x1, max(0, sx - 3), min(W, tx + 3 + 1)) {
-        REP3 (y, max(0, sy - 3), min(H, ty + 3 + 1)) {
-            REP3 (x2, x1, min(W, tx + 3 + 1)) {
-                path_composer path(sy, sx);
-                path.go_col(x1);
-                path.go_row(y);
-                path.go_col(x2);
-                path.go_row(ty);
-                path.go_col(tx);
-                use(path);
-            }
-        }
-    }
-
-    while (true) {
-        if (cands.empty()) {
-            return solve_with_simple(sy, sx, ty, tx);
-        }
-        if (cands.back()) {
-            break;
-        }
-        cands.pop_back();
-    }
-    return *cands.back();
-}
-
 class base_predictor {
     vector<int64_t> actual_score;
     array<array<vector<int>, W - 1>, H> used_hr;
@@ -445,8 +131,6 @@ class base_predictor {
         array<int, W> sep_y;
         array<int64_t, H> row1, row2;
         array<int64_t, W> col1, col2;
-        array<array<int64_t, W - 1>, H> dhr;
-        array<array<int64_t, H - 1>, W> dvr;
         vector<int64_t> predicted_score;
         int64_t loss;
     };
@@ -459,12 +143,12 @@ class base_predictor {
         array<array<int64_t, H - 1>, W> vr;
         REP (y, H) {
             REP (x, W - 1) {
-                hr[y][x] = (x < state.sep_x[y] ? state.row1 : state.row2)[y] + state.dhr[y][x];
+                hr[y][x] = (x < state.sep_x[y] ? state.row1 : state.row2)[y];
             }
         }
         REP (x, W) {
             REP (y, H - 1) {
-                vr[x][y] = (y < state.sep_y[x] ? state.col1 : state.col2)[x] + state.dvr[x][y];
+                vr[x][y] = (y < state.sep_y[x] ? state.col1 : state.col2)[x];
             }
         }
         return {hr, vr};
@@ -478,12 +162,6 @@ public:
         fill(ALL(cur.row2), 5000);
         fill(ALL(cur.col1), 5000);
         fill(ALL(cur.col2), 5000);
-        REP (y, H) {
-            fill(ALL(cur.dhr[y]), 0);
-        }
-        REP (x, W) {
-            fill(ALL(cur.dvr[x]), 0);
-        }
         cur.loss = 0;
 
         best = cur;
@@ -535,24 +213,22 @@ public:
 
             constexpr int VALUE_MIN = 1000;
             constexpr int VALUE_MAX = 9000;
-            constexpr int D = 10;
 
             auto probability = [&](int64_t delta) -> double {
                 constexpr double boltzmann = 0.0001;
                 return exp(- boltzmann * delta / temprature);
             };
 
-            double choice = uniform_real_distribution<double>()(gen);
-            if (choice < 0.8) {
+            if (bernoulli_distribution(0.8)(gen)) {
                 int i = uniform_int_distribution<int>(0, 4 - 1)(gen);
                 int z = uniform_int_distribution<int>(0, H - 1)(gen);
                 int64_t d = uniform_int_distribution<int>(-200, 200)(gen);
 
                 auto& value = (i == 0 ? cur.row1 : i == 1 ? cur.row2 : i == 2 ? cur.col1 : cur.col2)[z];
-                if (value + d < VALUE_MIN + D) {
-                    d = VALUE_MIN + D - value;
-                } else if (VALUE_MAX - D < value + d) {
-                    d = VALUE_MAX - D - value;
+                if (value + d < VALUE_MIN) {
+                    d = VALUE_MIN - value;
+                } else if (VALUE_MAX < value + d) {
+                    d = VALUE_MAX - value;
                 }
                 int l = 0;
                 int r = H - 1;
@@ -579,11 +255,6 @@ public:
 
                 if (delta <= 0 or bernoulli_distribution(probability(delta))(gen)) {
                     // accept
-#ifdef VERBOSE
-                    if (delta > 0) {
-                        cerr << "bad move " << probability(delta) << endl;
-                    }
-#endif  // VERBOSE
                     value += d;
                     REP3 (w, l, r) {
                         for (int j : used[w]) {
@@ -593,41 +264,6 @@ public:
                         }
                     }
                 }
-
-            } else if (choice < 0.9) {
-                bool is_row = bernoulli_distribution(0.5)(gen);
-                int z1 = uniform_int_distribution<int>(0, H - 1)(gen);
-                int z2 = uniform_int_distribution<int>(0, W - 2)(gen);
-                int64_t d = uniform_int_distribution<int>(-10, 10)(gen);
-
-                auto& value = (is_row ? cur.dhr : cur.dvr)[z1][z2];
-                if (value + d < - D) {
-                    d = - D - value;
-                } else if (D < value + d) {
-                    d = D - value;
-                }
-                auto& used = (is_row ? used_hr : used_vr)[z1][z2];
-                if (used.empty()) {
-                    continue;
-                }
-
-                int64_t delta = 0;
-                for (int j : used) {
-                    delta -= abs(cur.predicted_score[j] - actual_score[j]);
-                    delta += abs(cur.predicted_score[j] + d - actual_score[j]);
-                }
-                if (delta <= 0 or bernoulli_distribution(probability(delta))(gen)) {
-                    // accept
-#ifdef VERBOSE
-                    if (delta > 0) {
-                        cerr << "bad move " << probability(delta) << endl;
-                    }
-#endif  // VERBOSE
-                    for (int j : used) {
-                        cur.predicted_score[j] += d;
-                    }
-                }
-
 
             } else {
                 bool is_row = bernoulli_distribution(0.5)(gen);
@@ -659,11 +295,6 @@ public:
 
                 if (delta <= 0 or bernoulli_distribution(probability(delta))(gen)) {
                     // accept
-#ifdef VERBOSE
-                    if (delta > 0) {
-                        cerr << "bad move " << probability(delta) << endl;
-                    }
-#endif  // VERBOSE
                     if (d == -1) {
                         for (int j : used[sep - 1]) {
                             cur.loss -= abs(cur.predicted_score[j] - actual_score[j]);
@@ -712,50 +343,8 @@ void solve(function<tuple<int, int, int, int> ()> read, function<int64_t (const 
         array<array<int64_t, H - 1>, W> vr;
         tie(hr, vr) = predictor.get();
 
-        // make the relatie relation between s and t simple
-        bool is_flipped_vr = false;
-        auto flip_vr = [&]() {
-            is_flipped_vr = not is_flipped_vr;
-            sy = H - sy - 1;
-            ty = H - ty - 1;
-            REP (i, path.size()) {
-                path[i].first = W - path[i].first - 1;
-            }
-            reverse(ALL(hr));
-            REP (y, H) {
-                reverse(ALL(vr[y]));
-            }
-        };
-        bool is_flipped_hr = false;
-        auto flip_hr = [&]() {
-            is_flipped_hr = not is_flipped_hr;
-            sx = W - sx - 1;
-            tx = W - tx - 1;
-            REP (i, path.size()) {
-                path[i].second = W - path[i].second - 1;
-            }
-            reverse(ALL(vr));
-            REP (x, W) {
-                reverse(ALL(hr[x]));
-            }
-        };
-        if (sy > ty) {
-            flip_vr();
-        }
-        if (sx > tx) {
-            flip_hr();
-        }
-
         // solve
         path = solve_with_dijkstra(sy, sx, ty, tx, hr, vr);
-
-        // stop flipping
-        if (is_flipped_hr) {
-            flip_hr();
-        }
-        if (is_flipped_vr) {
-            flip_vr();
-        }
 
         // output
         int64_t score = write(get_command_from_path(path));
