98fc87f6834632edb53defc2560d1c8e76bbd0e2
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-05-28 11:05:56 +0900

    M = 1 や M = 2 だけの場合も見るように

--- scripts/dashboard.sh
+++ scripts/dashboard.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-k=${1:-50}
 mkdir -p log
+for M in 1 2 ; do
+    average=$(python3 scripts/measure.py --count=200 -M $M 2>&1 | tee log/M${M}.txt | tail -n 1 | cut -d = -f 2)
+    echo M = $M: $average
+done
 for D in 100 300 600 900 1200 1500 1800 2000 ; do
     for M in 1 2 ; do
-        average=$(python3 scripts/measure.py --count=$k -M $M -D $D 2>&1 | tee log/M${M}D${D}.txt | tail -n 1 | cut -d = -f 2)
+        average=$(python3 scripts/measure.py -M $M -D $D 2>&1 | tee log/M${M}D${D}.txt | tail -n 1 | cut -d = -f 2)
         echo M = $M, D = $D: $average
     done
 done
